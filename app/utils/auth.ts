f568fb2c850af8b24caff4b8df17bca0673f9154
Titus Nguyen <quannh.netalik@gmail.com>
2024-12-30 18:39:30 +0700

    prisma model

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,7 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Nodemailer from "next-auth/providers/nodemailer";
+import prisma from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     Nodemailer({
       server: {
